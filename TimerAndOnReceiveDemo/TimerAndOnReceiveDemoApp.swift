1f2b726f9e6e499c8e3da173a5aac5be4648cfa7
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 13:06:04 -0600

    Initial Commit

---
+++ TimerAndOnReceiveDemo/TimerAndOnReceiveDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAndOnReceiveDemoApp.swift
+//  TimerAndOnReceiveDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerAndOnReceiveDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
